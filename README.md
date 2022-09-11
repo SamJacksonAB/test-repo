e33121aa978eadcaed2edb32bcbe50f50821b742
SamJacksonAB <57988294+SamJacksonAB@users.noreply.github.com>
2022-09-11 12:17:00 +0100

    My first repo update
    I took a graph and made it better

--- README.md
+++ README.md
@@ -1,3 +1,50 @@
+## My test repo
+
 This test-project is part of a Github tutorial at the Data Science Garage Bootcamp
 
 https://dsgarage.netlify.app/bootcamp/
+
+
+## Bad graph
+
+### Bad graph R code
+plot(y = data$r/data$n,
+      x = data$var)
+      
+![](bad-graph.png)
+
+### Better graph R code
+
+#### Set plot background colour
+par(bg = "grey30")
+
+#### Create graph
+plot(y = data$r/data$n, 
+     x = data$var,
+     xlab = "Cane variety (n = 45)",
+     ylab = "Proportion infected",
+     col = data$var,
+     pch = 19,
+     cex = .5)
+     
+#### Calculate mean for each variety and add to graph
+data$var <- factor(data$var)
+var_mean <- aggregate(x = data$r/data$n,
+                      by = list(variety = data$var),
+                      FUN = mean)
+var_mean
+points(x = var_mean$variety,
+       y = var_mean$x,
+       col = var_mean$variety,
+       pch = 19,
+       cex = 1.5)
+       
+#### Add legend to graph
+legend(x = 28, y = 0.9,
+       legend = c("Variety mean", "Raw data"),
+       pch = c(19, 19),
+       pt.cex = c(1.5, .5),
+       col = c("black", "black"),
+       bty = "n")
+       
+![](good-graph.png)
--- script-3.3-reposcript.R
+++ script-3.3-reposcript.R
@@ -11,7 +11,7 @@
 
 ## 01 Setup ####
 # Get the data set up
-mydir <- "D:/Dropbox/git/DSgarage/public/data" # << change to yours!
+mydir <- "C:/Users/samjackson/Desktop/Other/PgC data science/Bootcamp/test-repo"
 setwd(mydir)
 
 library(openxlsx)
@@ -21,8 +21,35 @@ data <- read.xlsx("3.3-cane.xlsx")
 # Make a plot showing the proportion of diseased stems per plot
 # (variable "r" / variable "n")
 # as a function of cane variety (variable "var")
+
+# Set plot background colour
+par(bg = "grey30")
+
+# Create graph
 plot(y = data$r/data$n, 
-     x = data$var)
+     x = data$var,
+     xlab = "Cane variety (n = 45)",
+     ylab = "Proportion infected",
+     col = data$var,
+     pch = 19,
+     cex = .5)
 
+# Calculate mean for each variety and add to graph
+data$var <- factor(data$var)
+var_mean <- aggregate(x = data$r/data$n,
+                      by = list(variety = data$var),
+                      FUN = mean)
+var_mean
+points(x = var_mean$variety,
+       y = var_mean$x,
+       col = var_mean$variety,
+       pch = 19,
+       cex = 1.5)
 
-
+# Add legend to graph
+legend(x = 28, y = 0.9,
+       legend = c("Variety mean", "Raw data"),
+       pch = c(19, 19),
+       pt.cex = c(1.5, .5),
+       col = c("black", "black"),
+       bty = "n")
